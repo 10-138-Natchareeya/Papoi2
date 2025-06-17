b641bb92d5ce0afaa40a87ffe4cba85f7f4550dc
Natchareeya Sutthiprapa <natchareeya635@gmail.com>
2025-06-17 22:10:53 +0700

    Add initial code

--- app.py
+++ app.py
@@ -86,4 +86,6 @@ def handle_message(event):
     line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply_text))
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    import os
+    port = int(os.environ.get("PORT", 5000))  # ใช้พอร์ตจาก env variable หรือดีฟอลต์เป็น 5000
+    app.run(host="0.0.0.0", port=port)
